db0e8f8a758236e961c523bb6272a67e876d878b
Mervin Nguyen <mervin1@uci.edu>
2024-12-05 13:21:33 -0800

    Project requirements

--- 5thDayChristmas.py
+++ 5thDayChristmas.py
@@ -1,2 +1,7 @@
 # December 5th - Python project 
-#Engineer: Mervin Nguyen
+# Engineer: Mervin Nguyen
+
+#Steps
+#1: Create a new directory for your project
+#2: Inside the directory, create a python script
+#3: Add to generate a greeting card with a festive ASCII border.
