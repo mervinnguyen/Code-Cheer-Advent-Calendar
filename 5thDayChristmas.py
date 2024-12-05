cf4e96fcaf2e2fa7afa5ac4dd8666fc6834ade99
Mervin Nguyen <mervin1@uci.edu>
2024-12-05 13:24:55 -0800

    Correct typo

--- 5thDayChristmas.py
+++ 5thDayChristmas.py
@@ -6,9 +6,9 @@
 #2: Inside the directory, create a python script
 #3: Add to generate a greeting card with a festive ASCII border.
 
-def generateGreetinCard():
+def generateGreetingCard():
 #create ASCII border
 
 if __name__ == "__main__":
     message = "Happy holidays! Wishing you joy and peace."
-    generateGreetinCard(message)
+    generateGreetingCard(message)
