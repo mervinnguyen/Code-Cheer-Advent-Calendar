4f56b9eb7d854422ec39e2ebd7cb51a959b420a1
Mervin Nguyen <mervin1@uci.edu>
2024-12-05 13:24:00 -0800

    print out message

--- 5thDayChristmas.py
+++ 5thDayChristmas.py
@@ -5,3 +5,9 @@
 #1: Create a new directory for your project
 #2: Inside the directory, create a python script
 #3: Add to generate a greeting card with a festive ASCII border.
+
+def generateGreetinCard():
+
+if __name__ == "__main__":
+    message = "Happy holidays! Wishing you joy and peace."
+    generateGreetinCard(message)
