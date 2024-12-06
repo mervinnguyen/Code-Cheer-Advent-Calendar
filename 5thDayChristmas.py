84cba88c4e10d3fe912081eb0e46bbd229aa6c5e
Mervin's PC remote repository <mervinnguyen123@gmail.com>
2024-12-05 17:00:31 -0800

    Testing version control on PC

--- 5thDayChristmas.py
+++ 5thDayChristmas.py
@@ -6,13 +6,15 @@
 #2: Inside the directory, create a python script
 #3: Add to generate a greeting card with a festive ASCII border.
 
+#testing version control on PC
+
 def generateGreetingCard():
 #create ASCII border
     border "*" * (len(message) + 4)
     print(border)
     print(f"* {message} *")
     print(border)
-    
+
 if __name__ == "__main__":
     message = "Happy holidays! Wishing you joy and peace."
     generateGreetingCard(message)
