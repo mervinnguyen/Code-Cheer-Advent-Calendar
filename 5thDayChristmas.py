90f83aa685c9168f9cfb2a59b9d204f49aa58bf6
Mervin Nguyen <mervinnguyen123@gmail.com>
2024-12-05 17:08:20 -0800

    testing

--- 5thDayChristmas.py
+++ 5thDayChristmas.py
@@ -6,7 +6,7 @@
 #2: Inside the directory, create a python script
 #3: Add to generate a greeting card with a festive ASCII border.
 
-#testing version control on PC
+#changing the name of local device
 
 def generateGreetingCard():
 #create ASCII border
