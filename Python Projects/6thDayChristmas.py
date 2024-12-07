c40c2f25984b83616a50f90b1aaac0d1b94960a9
Mervin Nguyen <mervinnguyen123@gmail.com>
2024-12-07 09:14:07 -0800

    Add initial drawing setup and positioning for turtle graphics

--- Python Projects/6thDayChristmas.py
+++ Python Projects/6thDayChristmas.py
@@ -12,3 +12,15 @@ bgcolor('Christmas.jpg')
 
 mixer.init() # Initialize the mixer for music
 mixer.music.load('') # Load the music file (must me mp3 file)
+
+penup()
+goto(0, -130)
+pendown()
+left(90)
+forward(3*n)
+color('black', 'red')
+begin_fill()
+left(126)   #for position towards left side
+
+for i in range(5):
+    forward(n/5)
