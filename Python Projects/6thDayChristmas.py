d4f91796a3bc9c276b11c59866fec2d4a719e215
Mervin Nguyen <mervinnguyen123@gmail.com>
2024-12-07 09:11:02 -0800

    speed, pensize, background

--- Python Projects/6thDayChristmas.py
+++ Python Projects/6thDayChristmas.py
@@ -4,3 +4,8 @@ import random
 from pygame import mixer
 
 colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'white', 'black', 'pink', 'brown']
+
+n = 45
+speed(0)
+pensize(2)
+bgcolor('Christmas.jpg')
