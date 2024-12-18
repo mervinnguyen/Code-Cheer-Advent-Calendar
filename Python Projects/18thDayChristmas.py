0034583eb77d4825e69c6921cc1b83c782076bc5
Mervin Nguyen <mervin1@uci.edu>
2024-12-18 14:39:56 -0800

    Add logic to calculate elf name indices based on user's initials
    -Convert first and last name initials to uppercase
    -Calculate index for first name part using ASCII value and modulo operation
    -Calculate index for last name part using ASCII value and modulo operation

--- Python Projects/18thDayChristmas.py
+++ Python Projects/18thDayChristmas.py
@@ -1,2 +1,9 @@
 def generateElfName(firstName, lastName):
-    firstName = ["Jolly", "Merry", "Sparkle", "Twinkle", "Snowy", "Frosty", "Candy", "Ginger"]
+    firstNamePart = ["Jolly", "Merry", "Sparkle", "Twinkle", "Snowy", "Frosty", "Candy", "Ginger"]
+    lastNamePart = ["Bells", "Snowflake", "Icicle", "Twinkletoes", "Sugarplum", "Peppermint", "Jingle", "Holly"]
+    
+    firstInitial = firstName[0].upper()
+    lastInitial = lastName[0].upper()
+
+    firstIndex = (ord(firstInitial) - ord('A')) % len(firstNamePart)
+    lastIndex = (ord(lastInitial) - ord('A')) % len(lastNamePart)
