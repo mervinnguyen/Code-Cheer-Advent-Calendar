3bf6c00317440b1b2c34e31ff40bf2b7b5778aa7
Mervin Nguyen <mervin1@uci.edu>
2024-12-19 15:00:44 -0800

    Add glowing and unwrapping effect
    -Create index.html with button element
    -Add styles.css with styles for body and button
    -Implement glowing effect on button click using JS
    -Add unwrapping effect on button hover using CSS transition
    -Include script.js to toggle glow class on button click

--- Python Projects/18thDayChristmas.py
+++
@@ -1,9 +0,0 @@
-def generateElfName(firstName, lastName):
-    firstNamePart = ["Jolly", "Merry", "Sparkle", "Twinkle", "Snowy", "Frosty", "Candy", "Ginger"]
-    lastNamePart = ["Bells", "Snowflake", "Icicle", "Twinkletoes", "Sugarplum", "Peppermint", "Jingle", "Holly"]
-    
-    firstInitial = firstName[0].upper()
-    lastInitial = lastName[0].upper()
-
-    firstIndex = (ord(firstInitial) - ord('A')) % len(firstNamePart)
-    lastIndex = (ord(lastInitial) - ord('A')) % len(lastNamePart)
