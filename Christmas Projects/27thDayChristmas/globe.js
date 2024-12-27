447a8562a264093bcc20c58d7860e8c990e004f2
Mervin Nguyen <mervinnguyen123@gmail.com>
2024-12-27 10:42:10 -0800

    function to createSnowflake

--- Christmas Projects/27thDayChristmas/globe.js
+++ Christmas Projects/27thDayChristmas/globe.js
@@ -1,3 +1,8 @@
 function createSnowFlake(){
-    
+    const snowflake = document.createElement('div');
+    snowflake.classList.add('snowflake');
+    snowflake.style.left = Math.random() * 100 + '%';
+    snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
+    snowflake.style.opacity = Math.random();
+    snowflake.style.fontSize = Math.random() * 10 + 10 + 'px';
 }
