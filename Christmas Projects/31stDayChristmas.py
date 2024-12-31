7ba112fdec84a22991f922c1bce1abb3a0bbb522
Mervin Nguyen <mervin1@uci.edu>
2024-12-31 09:40:24 -0800

    random shuffle names

--- Christmas Projects/31stDayChristmas.py
+++ Christmas Projects/31stDayChristmas.py
@@ -3,3 +3,9 @@ import random
 def pairPeople(names):
     if len(names) < 2:
         raise ValueError("Need at least two people to pair up")
+    
+    givers = names[:]
+    receivers = names[:]
+
+    random.shuffle(givers)
+    random.shuffle(receivers)
